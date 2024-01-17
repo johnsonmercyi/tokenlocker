9bda37b62b55d80998c1fc58917af3827755983a
johnsonmercyi <johnsonmercy.ng@gmail.com>
2024-01-17 15:43:10 +0100

    more responsiveness tweak

--- tokenlockerui/src/components/Layout/Header.jsx
+++ tokenlockerui/src/components/Layout/Header.jsx
@@ -4,32 +4,37 @@ import { useRouter } from "next/router";
 import React from "react";
 import { Menu } from "semantic-ui-react";
 import UIButton from "../UI/UIButton/UIButton";
+import styles from "./Header.module.css";
 
 const Header = ({ ...props }) => {
   const router = useRouter();
   const { manager } = router.query;
 
   return (
-    <div style={{
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginBottom: "2rem",
-    }}>
-      <Link href={`/${manager}/tokens`} style={{ color: "white" }}>
+    <div
+      className={styles.header}
+      style={{
+        width: "100%",
+        marginBottom: "2rem",
+      }}>
+      <Link href={`/${manager}/tokens`} style={{ color: "white" }} className={styles.appLogo}>
         <AppLogo
           title={"TheLockerSpace"}
           width={"3rem"}
           height={"3rem"} />
       </Link>
 
-      <div style={{
-        width: "fit-content",
+      <div 
+      className={styles.buttonsWrapper}
+      style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
       }}>
-        <Link href={`/${manager}/tokens`} style={{ color: "white" }}>
+        <Link
+          className={styles.spaceBtn}
+          href={`/${manager}/tokens`}
+          style={{ color: "white" }}>
           <UIButton
             rounded_left
             content={"Space"}
@@ -37,7 +42,10 @@ const Header = ({ ...props }) => {
             labelPosition={"left"} />
         </Link>
 
-        <Link href={`/${manager}/tokens/new`} style={{ color: "white" }}>
+        <Link
+          href={`/${manager}/tokens/new`}
+          className={styles.lockNewBtn}
+          style={{ color: "white", width: "6rem" }}>
           <UIButton
             rounded_right
             content={"+"} />
